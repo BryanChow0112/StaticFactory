14871289c7bc7378d73aa167f4baa4db0f38061d
ethan <ewat0010@student.monash.edu>
2024-04-28 19:59:52 +1000

    Changes to Puddle Implemented puddle to implement Consumable as is important for new requirement. Also implemented required consumable methods

--- src/game/grounds/Puddle.java
+++ src/game/grounds/Puddle.java
@@ -1,9 +1,21 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import game.scraps.Consumable;
 
-public class Puddle extends Ground {
+public class Puddle extends Ground implements Consumable {
     public Puddle() {
         super('~');
+    }
+
+    @Override
+    public String handleConsume(Actor actor) {
+        return null;
+    }
+
+    @Override
+    public String getConsumableDescription() {
+        return null;
     }
 }
