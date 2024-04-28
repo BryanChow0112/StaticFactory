e702177834023a07f7ca0883597489f41df53956
mdig0003 <96048719+mdig0003@users.noreply.github.com>
2024-04-28 22:23:14 +1000

    Added an override method for handleConsume which defines how the scrap behaves.

--- src/game/scraps/special/JarOfPickles.java
+++ src/game/scraps/special/JarOfPickles.java
@@ -5,6 +5,7 @@ import edu.monash.fit2099.engine.actors.Actor;
 import game.actions.ConsumeAction;
 import game.scraps.Consumable;
 import game.scraps.Scrap;
+import game.utils.RandomUtils;
 
 public class JarOfPickles extends Scrap implements Consumable {
     private static final int HIT_POINTS = 1;
@@ -18,6 +19,22 @@ public class JarOfPickles extends Scrap implements Consumable {
         ActionList actionList = new ActionList();
         actionList.add(new ConsumeAction(this));
         return actionList;
+    }
+
+    @Override
+    public String handleConsume(Actor actor) {
+
+        if (RandomUtils.getRandomDouble() <= 0.5){
+            // if less than 50% chance, it decreases player hp by 1
+            actor.heal(HIT_POINTS*-1);
+            actor.removeItemFromInventory(this);
+            return actor + "consumes Jar of Pickles to heal " + HIT_POINTS*-1 + " hit points.";
+        } else {
+            // if greater than 50% chance, it increases player hp by 1
+            actor.heal(HIT_POINTS);
+            actor.removeItemFromInventory(this);
+            return actor + "consumes Jar of Pickles to heal " + HIT_POINTS + " hit points.";
+        }
     }
 
 }
