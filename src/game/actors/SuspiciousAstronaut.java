8b87c68b4223c52e3dc77c86f15950c1935d5d12
ethan <ewat0010@student.monash.edu>
2024-04-28 20:10:11 +1000

    Changes SuspiciousAstronaut Added required methods for Suspicious Astronaut as it implements spawnable

--- src/game/actors/SuspiciousAstronaut.java
+++ src/game/actors/SuspiciousAstronaut.java
@@ -4,4 +4,13 @@ import edu.monash.fit2099.engine.actors.Actor;
 
 public class SuspiciousAstronaut extends Enemy implements Spawnable{
 
+    @Override
+    public Actor create() {
+        return null;
+    }
+
+    @Override
+    public double getSpawnChance() {
+        return 0;
+    }
 }
