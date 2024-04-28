4310cb42ecd4d5cdd682b2e4d4e1c0e7e8e82442
ethan <ewat0010@student.monash.edu>
2024-04-28 20:02:49 +1000

    Changes to Puddle Added new allowable actions Override method that allows a new actions for the actor to consume the puddle if it is stood on the puddle

--- src/game/grounds/Puddle.java
+++ src/game/grounds/Puddle.java
@@ -1,10 +1,15 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
 import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actions.ConsumeAction;
 import game.scraps.Consumable;
+
+import java.util.Objects;
 
 public class Puddle extends Ground implements Consumable {
     private final int INCREASE_HP = 1;
@@ -21,5 +26,13 @@ public class Puddle extends Ground implements Consumable {
     @Override
     public String getConsumableDescription() {
         return "increase max health by " + INCREASE_HP + " HP" + ".";
+    }
+    @Override
+    public ActionList allowableActions(Actor actor, Location location, String direction){
+        ActionList actionList = new ActionList();
+        if (Objects.equals(direction, "")){
+            actionList.add(new ConsumeAction(this));
+        }
+        return actionList;
     }
 }
