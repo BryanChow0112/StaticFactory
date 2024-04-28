dd2acb76e32e5ebbfd23c8819bb2965b862e0ea0
ethan <ewat0010@student.monash.edu>
2024-04-28 20:11:35 +1000

    Changes SuspiciousAstronaut Added Constructor required for suspcious astronaout and attribute for player

--- src/game/actors/SuspiciousAstronaut.java
+++ src/game/actors/SuspiciousAstronaut.java
@@ -1,8 +1,23 @@
 package game.actors;
 
 import edu.monash.fit2099.engine.actors.Actor;
+import game.behaviours.AttackBehaviour;
+import game.behaviours.WanderBehaviour;
 
 public class SuspiciousAstronaut extends Enemy implements Spawnable{
+    Player player;
+    /**
+     * Constructs a new Enemy object.
+     *
+     * @param name        The name of the enemy.
+     * @param displayChar The character used to represent the enemy on the display.
+     * @param hitPoints   The initial hit points of the enemy.
+     */
+    public SuspiciousAstronaut(Player player) {
+        super("Suspicious Astronaut", 'ඞ', 99);
+        this.player = player;
+        this.addBehaviour(new AttackBehaviour());
+        this.addBehaviour(new WanderBehaviour());
 
     @Override
     public Actor create() {
