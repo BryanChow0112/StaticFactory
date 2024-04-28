3f4b602f43d4541b5f5e9a9bdb7c2fd50c4ed228
bcho0034 <bcho0034@student.monash.edu>
2024-04-28 19:21:43 +1000

    Implement FollowBehaviour class
    This commit introduces the FollowBehaviour class, which implements the Behaviour interface. The FollowBehaviour is responsible for determining the appropriate action for an Actor to follow another target Actor on the game map. The AlienBug class is also refactored to include FollowBehaviour. The Application class is modified for testing.
    
    FollowBehaviour
    - Constructor that takes the target Actor to follow
    - Implemented the getAction() method, which calculates the shortest path towards the target Actor and returns a MoveActorAction to move in that direction
    - Implemented a distance() method that computes the Manhattan distance between two locations
    - Referred to mars demo
    
    AlienBug
    - Introduced new playerToFollow variable
    - Added FollowBehaviour with priority 0
    
    Application
    - Added craters spawning AlienBugs for testing

--- src/game/Application.java
+++ src/game/Application.java
@@ -20,6 +20,7 @@ import game.scraps.regular.MetalSheet;
 /**
  * The main class to start the game.
  * Created by:
+ *
  * @author Adrian Kristanto
  * Modified by:
  * @author Lachlan MacPhee
@@ -34,21 +35,21 @@ public class Application {
                 new Wall(), new Floor(), new Puddle(), new Tree(), new Sapling());
 
         List<String> map = Arrays.asList(
-                        "...~~~~.........~~~...........",
-                        "...~~~~.................t.....",
-                        "...~~~........................",
-                        "..............................",
-                        ".............#####............",
-                        "...T.........#___#...........~",
-                        ".............#___#..t.......~~",
-                        ".............##_##.........~~~",
-                        ".................~~........~~~",
-                        "................~~~~.......~~~",
-                        ".............~~~~~~~........~~",
-                        "......~.....~~~~~~~~.........~",
-                        ".....~~~...~~~~~~~~~..........",
-                        "..t..~~~~~~~~~~~~~~~~........~",
-                        ".....~~~~~~~~~~~~~~~~~~~....~~");
+                "...~~~~.........~~~...........",
+                "...~~~~.................t.....",
+                "...~~~........................",
+                "..............................",
+                ".............#####............",
+                "...T.........#___#...........~",
+                ".............#___#..t.......~~",
+                ".............##_##.........~~~",
+                ".................~~........~~~",
+                "................~~~~.......~~~",
+                ".............~~~~~~~........~~",
+                "......~.....~~~~~~~~.........~",
+                ".....~~~...~~~~~~~~~..........",
+                "..t..~~~~~~~~~~~~~~~~........~",
+                ".....~~~~~~~~~~~~~~~~~~~....~~");
 
         GameMap gameMap = new GameMap(groundFactory, map);
         world.addGameMap(gameMap);
@@ -62,26 +63,31 @@ public class Application {
             }
         }
 
+        // Add player
+        Player player = new Player("Intern", '@', 4);
+        world.addPlayer(player, gameMap.at(15, 6));
+
+        // Add large bolt and metal sheet
         Item largeBolt = new LargeBolt();
         gameMap.at(8, 7).addItem(largeBolt);
 
         Item metalSheet = new MetalSheet();
         gameMap.at(9, 7).addItem(metalSheet);
 
+        // Add metal pipe
         Item metalPipe = new MetalPipe();
         gameMap.at(10, 7).addItem(metalPipe);
 
+        // Create craters spawning HuntsmanSpiders
         Crater craterOne = new Crater(new HuntsmanSpider());
         gameMap.at(26, 4).setGround(craterOne);
 
-        Crater craterTwo = new Crater(new HuntsmanSpider());
+        // Create craters spawning AlienBugs
+        Crater craterTwo = new Crater(new AlienBug(player));
         gameMap.at(6, 5).setGround(craterTwo);
 
-        Crater craterThree = new Crater(new HuntsmanSpider());
+        Crater craterThree = new Crater(new AlienBug(player));
         gameMap.at(26, 12).setGround(craterThree);
-
-        Player player = new Player("Intern", '@', 4);
-        world.addPlayer(player, gameMap.at(15, 6));
 
         world.run();
     }
--- src/game/actors/AlienBug.java
+++ src/game/actors/AlienBug.java
@@ -1,25 +1,31 @@
 package game.actors;
 
 import edu.monash.fit2099.engine.actors.Actor;
+import game.behaviours.FollowBehaviour;
 import game.types.Ability;
 
 public class AlienBug extends Enemy implements Spawnable {
 
+
+    private final Player playerToFollow;
+
     /**
      * Constructs a new AlienBug object.
      */
-    public AlienBug() {
+    public AlienBug(Player playerToFollow) {
         super("Alien Bug", 'a', 2);
+        this.addBehaviour(0, new FollowBehaviour(playerToFollow));
         this.addCapability(Ability.PICK_UP_SCRAP);
         this.addCapability(Ability.ENTER_SPACESHIP);
+        this.playerToFollow = playerToFollow;
 
-        // TODO: Implement PickUpScrapBehaviour and FollowBehaviour
+        // TODO: Implement PickUpScrapBehaviour
         // TODO: Implement naming “Feature-XXX”, where XXX is three random digits
     }
 
     @Override
     public Actor create() {
-        return new AlienBug();
+        return new AlienBug(this.playerToFollow);
     }
 
     @Override
