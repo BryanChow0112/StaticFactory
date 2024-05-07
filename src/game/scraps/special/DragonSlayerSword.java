8d91087dfa38cd03e42de7f46a056f7f3e527738
bcho0034 <bcho0034@student.monash.edu>
2024-05-07 14:47:01 +1000

    Refactor weapon hierarchy by removing unnecessary WeaponScrap abstraction
    This commit removes WeaponScrap class as it was an unnecessary abstraction layer between WeaponItem and its concrete subclasses. By removing it, the weapon hierarchy is simplified, making the codebase more maintainable and easier to understand.
    
    - Deleted the WeaponScrap abstract class that extended WeaponItem from game engine
    - Updated all classes that previously extended WeaponScrap to directly extend WeaponItem
    - Refactored allowableActions() method in weapon subclasses to return AttackAction with weapon
    - Removed references to WeaponScrap class throughout the codebase

--- src/game/scraps/special/DragonSlayerSword.java
+++ src/game/scraps/special/DragonSlayerSword.java
@@ -1,16 +1,39 @@
 package game.scraps.special;
 
+import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
-import game.scraps.WeaponScrap;
+import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+import game.actions.AttackAction;
 import game.types.Buyable;
+import game.types.Status;
 import game.utils.BuyUtils;
 import game.utils.RandomUtils;
 
-public class DragonSlayerSword extends WeaponScrap implements Buyable {
+public class DragonSlayerSword extends WeaponItem implements Buyable {
     private static final int WORTH_IN_CREDITS = 100;
 
     public DragonSlayerSword() {
         super("Dragon Slayer Sword", 'x', 50, "slices", 75);
+    }
+
+    /**
+     * List of allowable actions that the item allows its owner do to other actor.
+     *
+     * @param otherActor the other actor
+     * @param location   the location of the other actor
+     * @return an unmodifiable list of Actions
+     */
+    @Override
+    public ActionList allowableActions(Actor otherActor, Location location) {
+        ActionList actions = new ActionList();
+        String locationString = "(" + location.x() + ", " + location.y() + ")";
+
+        if (otherActor.hasCapability(Status.HOSTILE_TO_PLAYER)) {
+            // Allow attacking for other actor (Enemy) using MetalPipe as weapon
+            actions.add(new AttackAction(otherActor, locationString, this));
+        }
+        return actions;
     }
 
     @Override
