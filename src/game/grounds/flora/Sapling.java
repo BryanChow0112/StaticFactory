d0bbb3380a48efe797a079c5c917dde5397d7524
ethan <ewat0010@student.monash.edu>
2024-05-19 17:28:06 +1000

    Req 4 Inheritree Java Doc added for Mature and Sapling

--- src/game/grounds/flora/Sapling.java
+++ src/game/grounds/flora/Sapling.java
@@ -5,38 +5,72 @@ import game.scraps.special.Fruit;
 import game.scraps.special.SmallFruit;
 import game.types.Growable;
 
+/**
+ * Represents the sapling stage of inheritree
+ */
 public class Sapling extends FruitfulTree implements Growable {
-
+    /**
+     * The age of the sapling
+     */
     private int age;
+
+    /**
+     * Construct new instance of sapling
+     */
     public Sapling() {
         super('t');
     }
 
+    /**
+     * Method for the sapling to experience time
+     * @param location The location of the Ground
+     */
     @Override
     public void tick(Location location){
         produceFruit(location);
         grow(location);
     }
+
+    /**
+     * Method to get the fruit that can be spawned
+     * @return small fruit
+     */
     @Override
     public Fruit getFruit() {
         return new SmallFruit();
     }
 
+    /**
+     * % Chance of the fruit spawning
+     * @return int representing a percentage
+     */
     @Override
     public int getSpawnChance() {
         return 30;
     }
 
+    /**
+     * get the next stage once it ages enough
+     * @return next stage new YoungInheritree
+     */
     @Override
     public PlantBase nextStage() {
         return new YoungInheritree();
     }
 
+    /**
+     * Age that the Sapling grows
+     * @return int growAge
+     */
     @Override
     public int growAge() {
         return 6;
     }
 
+    /**
+     * Method for the sapling to grow each tick
+     * @param location of sapling
+     */
     @Override
     public void grow(Location location) {
         age++;
@@ -45,6 +79,10 @@ public class Sapling extends FruitfulTree implements Growable {
         };
     }
 
+    /**
+     * Check if the Sapling has matured
+     * @return boolean if matured
+     */
     @Override
     public boolean isMature() {
         return age >= growAge();
