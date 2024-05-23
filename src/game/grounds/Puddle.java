11ed2952593fee671fbae2139d54841270a2c128
bcho0034 <bcho0034@student.monash.edu>
2024-05-23 13:49:46 +1000

    Refactor actor check in Puddle's allowableActions
    This commit refactors the previous implementation which used an object equality check to determine if an actor was standing on the puddle which violates SOLID principles.
    
    - Replaced the object equality check with location.containsAnActor()

--- src/game/grounds/Puddle.java
+++ src/game/grounds/Puddle.java
@@ -1,6 +1,7 @@
 package game.grounds;
 
 import java.util.Objects;
+
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
@@ -9,6 +10,7 @@ import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
 import game.actions.ConsumeAction;
 import game.types.Consumable;
+import game.types.Status;
 
 
 /**
@@ -29,6 +31,7 @@ public class Puddle extends Ground implements Consumable {
 
     /**
      * Method that handles the consume action
+     *
      * @param actor The actor consuming the item.
      * @return string to be displayed for successful consume
      */
@@ -40,26 +43,32 @@ public class Puddle extends Ground implements Consumable {
 
     /**
      * Method that get all the allowable actions
-     * @param actor the Actor acting
-     * @param location the current Location
+     *
+     * @param actor     the Actor acting
+     * @param location  the current Location
      * @param direction the direction of the Ground from the Actor
      * @return list of actions that are allowed
      */
     @Override
-    public ActionList allowableActions(Actor actor, Location location, String direction){
+    public ActionList allowableActions(Actor actor, Location location, String direction) {
         ActionList actionList = new ActionList();
-        if (Objects.equals(direction, "")){
-            actionList.add(new ConsumeAction(this));
+        // Check if there is an actor standing on the puddle
+        if (location.containsAnActor()) {
+            // Check if the actor (presumably the intern) has the HOSTILE_TO_ENEMY capability
+            if (actor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
+                actionList.add(new ConsumeAction(this));
+            }
         }
         return actionList;
     }
 
     /**
      * toString method once it is consumed
+     *
      * @return string
      */
     @Override
-    public String toString(){
+    public String toString() {
         return "the puddle underneath the player";
     }
 }
