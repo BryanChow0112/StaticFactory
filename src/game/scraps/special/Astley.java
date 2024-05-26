99905e442cf62a8a53ed4419c1ecb3c3a63ba211
bcho0034 <bcho0034@student.monash.edu>
2024-05-26 16:41:46 +1000

    Implement conditional monologue options and random selection
    - Added checks for specific conditions in the tick method
    - Certain monologue options are only available when specific conditions are met
    - These conditional monologue options are added to the monologueOptions list
    - Updated the generateMonologue() method in Astley to randomly selects a monologue option from the monologueOptions list

--- src/game/scraps/special/Astley.java
+++ src/game/scraps/special/Astley.java
@@ -1,10 +1,13 @@
 package game.scraps.special;
 
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.Location;
 import game.types.Buyable;
 import game.types.Monologuer;
 import game.utils.BuyUtils;
+import game.utils.RandomUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +18,43 @@ public class Astley extends Item implements Buyable, Monologuer {
 
     private static final List<String> monologueOptions = new ArrayList<>();
 
+    // Initialise the monologue options list
+    static {
+        monologueOptions.add("The factory will never gonna give you up, valuable intern!");
+        monologueOptions.add("We promise we never gonna let you down with a range of staff benefits.");
+        monologueOptions.add("We never gonna run around and desert you, dear intern!");
+    }
+
     /**
      * Constructs a new Astley object.
      */
     public Astley() {
         super("Astley, an AI Device", 'z', true);
+    }
+
+    /**
+     * Inform a carried Item of the passage of time.
+     * This method is called once per turn, if the Item is being carried.
+     *
+     * @param currentLocation The location of the actor carrying this Item.
+     * @param actor           The actor carrying this Item.
+     */
+    @Override
+    public void tick(Location currentLocation, Actor actor) {
+
+        // if the intern has more than 10 items in their inventory
+        if (actor.getItemInventory().size() > 10) {
+            monologueOptions.add("We never gonna make you cry with unfair compensation.");
+        }
+        // if the intern carries more than 50 credits in their wallet
+        if (actor.getBalance() > 50) {
+            monologueOptions.add("Trust is essential in this business. We promise we never gonna say goodbye to a valuable intern like you.");
+        }
+        // if the intern’s health point is below 2
+        if (actor.getAttribute(BaseActorAttributes.HEALTH) < 2) {
+            monologueOptions.add("Don't worry, we never gonna tell a lie and hurt you, unlike those hostile creatures.");
+        }
+
     }
 
     @Override
@@ -34,6 +69,7 @@ public class Astley extends Item implements Buyable, Monologuer {
 
     @Override
     public String generateMonologue() {
-        return "";
+        return monologueOptions.get(RandomUtils.getRandomInt(monologueOptions.size()));
     }
+
 }
