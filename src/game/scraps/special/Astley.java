c76687b09c1d35e48d5a937919238415210b734a
bcho0034 <bcho0034@student.monash.edu>
2024-05-26 16:30:37 +1000

    Introduce Astley class
    - This class will represent the AI device that the intern can purchase and interact with
    - Further implementation for the Astley AI device functionality will follow

---
+++ src/game/scraps/special/Astley.java
@@ -0,0 +1,33 @@
+package game.scraps.special;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import game.types.Buyable;
+import game.utils.BuyUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Astley extends Item implements Buyable {
+
+    private static final int WORTH_IN_CREDITS = 50;
+
+    private static final List<String> monologueOptions = new ArrayList<>();
+
+    /**
+     * Constructs a new Astley object.
+     */
+    public Astley() {
+        super("Astley, an AI Device", 'z', true);
+    }
+
+    @Override
+    public String buy(Actor actor) {
+        return BuyUtils.buyItem(actor, this, WORTH_IN_CREDITS);
+    }
+
+    @Override
+    public int getCost() {
+        return WORTH_IN_CREDITS;
+    }
+}
