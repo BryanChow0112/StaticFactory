ceb9d506254c1f8d364433a6ccbb44e766b8c762
mdig0003 <96048719+mdig0003@users.noreply.github.com>
2024-05-27 11:16:42 +1000

    added javadoc for TeleportAction

--- src/game/actions/TeleportAction.java
+++ src/game/actions/TeleportAction.java
@@ -9,18 +9,36 @@ public class TeleportAction extends Action {
     private final String direction;
     private final Location destination;
 
+    /**
+     * Constructs a new TeleportAction object.
+     *
+     * @param direction The direction of the player.
+     * @param destination The location of the player.
+     */
     public TeleportAction(String direction, Location destination) {
         this.direction = direction;
         this.destination = destination;
     }
 
-
+    /**
+     * Executes the buy action for the given actor.
+     *
+     * @param actor The actor performing the buy action.
+     * @param map   The game map (not used in this action).
+     * @return The result message of the buy attempt.
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         map.moveActor(actor, destination);
         return actor + " arrives at " + destination.toString() + " in " + direction;
     }
 
+    /**
+     * Returns the menu description of this action.
+     *
+     * @param actor The actor performing the action.
+     * @return The menu description of this action.
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " travels to " + direction;
