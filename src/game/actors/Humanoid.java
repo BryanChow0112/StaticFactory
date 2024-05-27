cfc33d57a1c3964f5cb1a41d2226e7c9c56821db
Lachlan MacPhee <lmac0040@student.monash.edu>
2024-05-27 21:28:58 +1000

    lachlanmacphee: add the humanoid actor

---
+++ src/game/actors/Humanoid.java
@@ -0,0 +1,21 @@
+package game.actors;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.types.Ability;
+
+public class Humanoid extends Actor {
+
+    public Humanoid() {
+        super("Humanoid", 'H', 5);
+        this.addCapability(Ability.PURCHASE_ITEMS);
+    }
+
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        return null;
+    }
+}
