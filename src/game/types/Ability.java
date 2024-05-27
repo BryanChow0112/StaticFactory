eaead62d65345737eacc16b9696622769ed3eca1
Lachlan MacPhee <lmac0040@student.monash.edu>
2024-05-27 21:30:48 +1000

    lachlanmacphee: add the ability to purchase items

--- src/game/types/Ability.java
+++ src/game/types/Ability.java
@@ -5,5 +5,7 @@ package game.types;
  * Example #1: if the player is capable jumping over walls, you can attach Ability.WALL_JUMP to the player class
  */
 public enum Ability {
-    ENTER_SPACESHIP, CAN_PAY_SUBSCRIPTION
+    ENTER_SPACESHIP,
+    CAN_PAY_SUBSCRIPTION,
+    PURCHASE_ITEMS
 }
