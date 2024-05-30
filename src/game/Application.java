d1082098ddb330f8cb8e86b9a84fabf7776cdc9e
bcho0034 <bcho0034@student.monash.edu>
2024-05-30 13:03:25 +1000

    Improve code readability in Application class
    - Renamed variables with more descriptive names
    - Added comments to improve code readability

--- src/game/Application.java
+++ src/game/Application.java
@@ -39,7 +39,7 @@ public class Application {
                 new Wall(), new Floor(), new Puddle(), new Sprout()) {
         };
 
-        List<String> map = Arrays.asList(
+        List<String> polymorphia = Arrays.asList(
                 "...~~~~.........~~~...........",
                 "...~~~~..........,.....,......",
                 "...~~~.........,.,..,.........",
@@ -56,8 +56,9 @@ public class Application {
                 ".....~~~~~~~~~~~~~~~~........~",
                 ".....~~~~~~~~~~~~~~~~~~~....~~");
 
-        GameMap gameMap = new GameMap(groundFactory, map);
-        world.addGameMap(gameMap);
+        // Add Polymorphia game map
+        GameMap polymorphiaOne = new GameMap(groundFactory, polymorphia);
+        world.addGameMap(polymorphiaOne);
 
         List<String> staticFactory = Arrays.asList(
                 ".......",
@@ -70,10 +71,16 @@ public class Application {
                 ".......",
                 ".......",
                 ".......");
+
+        // Add Static Factory game map
         GameMap staticFactoryOne = new GameMap(groundFactory, staticFactory);
         world.addGameMap(staticFactoryOne);
 
-        List<String> newMoon = Arrays.asList(
+        // Add humanoid figure
+        Humanoid humanoid = new Humanoid();
+        staticFactoryOne.at(3, 9).addActor(humanoid);
+
+        List<String> refactorio = Arrays.asList(
                 "..........................~~~~",
                 "..........................~~~~",
                 "..........................~~~~",
@@ -89,8 +96,10 @@ public class Application {
                 "....................~~........",
                 ".............~~...............",
                 "............~~~~..............");
-        GameMap newMoonOne = new GameMap(groundFactory, newMoon);
-        world.addGameMap(newMoonOne);
+
+        // Add Refactorio game map
+        GameMap refactorioOne = new GameMap(groundFactory, refactorio);
+        world.addGameMap(refactorioOne);
 
         for (String line : FancyMessage.TITLE.split("\n")) {
             new Display().println(line);
@@ -101,7 +110,7 @@ public class Application {
             }
         }
 
-        // Setup Terminal
+        // Setup buyables for terminal
         ArrayList<Buyable> buyables = new ArrayList<>();
         buyables.add(new EnergyDrink());
         buyables.add(new DragonSlayerSword());
@@ -109,77 +118,73 @@ public class Application {
         buyables.add(new Astley());
         buyables.add(new THESEUS());
 
-        // added travel actions to a different terminal in Polymorphia
+        // Add travel actions for terminal in Polymorphia
         ArrayList<Action> travelActionPolymorphia = new ArrayList<>();
-        travelActionPolymorphia.add(new TeleportAction("Refactorio",newMoonOne.at(15,6)));
-        travelActionPolymorphia.add(new TeleportAction("Static Factory",staticFactoryOne.at(3,3)));
+        travelActionPolymorphia.add(new TeleportAction("Static Factory", staticFactoryOne.at(3, 3)));
+        travelActionPolymorphia.add(new TeleportAction("Refactorio", refactorioOne.at(15, 6)));
 
-        // placing terminals at different gameMaps
-        Terminal terminalOne = new Terminal(buyables,travelActionPolymorphia);
-        gameMap.at(15, 5).setGround(terminalOne);
+        // Place terminal at Polymorphia
+        Terminal terminalPolymorphia = new Terminal(buyables, travelActionPolymorphia);
+        polymorphiaOne.at(15, 5).setGround(terminalPolymorphia);
 
-        // added travel actions to a different terminal in Static Factory
+        // Add travel actions for terminal in Static Factory
         ArrayList<Action> travelActionStaticFactory = new ArrayList<>();
-        travelActionStaticFactory.add(new TeleportAction("Refactorio",newMoonOne.at(15,6)));
-        travelActionStaticFactory.add(new TeleportAction("Polymorphia",gameMap.at(15,6)));
+        travelActionStaticFactory.add(new TeleportAction("Polymorphia", polymorphiaOne.at(15, 6)));
+        travelActionStaticFactory.add(new TeleportAction("Refactorio", refactorioOne.at(15, 6)));
 
-        Terminal terminalTwo = new Terminal(buyables,travelActionStaticFactory);
-        staticFactoryOne.at(3, 2).setGround(terminalTwo);
+        // Place terminal at Static Factory
+        Terminal terminalStaticFactory = new Terminal(buyables, travelActionStaticFactory);
+        staticFactoryOne.at(3, 2).setGround(terminalStaticFactory);
 
-        // added travel actions to a different terminal in Refactorio
-        ArrayList<Action> travelActionNewMoonOne = new ArrayList<>();
-        travelActionNewMoonOne.add(new TeleportAction("Static Factory",staticFactoryOne.at(3,3)));
-        travelActionNewMoonOne.add(new TeleportAction("Polymorphia",gameMap.at(15,6)));
+        // Add travel actions for terminal in Refactorio
+        ArrayList<Action> travelActionRefactorio = new ArrayList<>();
+        travelActionRefactorio.add(new TeleportAction("Polymorphia", polymorphiaOne.at(15, 6)));
+        travelActionRefactorio.add(new TeleportAction("Static Factory", staticFactoryOne.at(3, 3)));
 
-        Terminal terminalThree = new Terminal(buyables,travelActionNewMoonOne);
-        newMoonOne.at(15, 5).setGround(terminalThree);
+        // Place terminal at Refactorio
+        Terminal terminalRefactorio = new Terminal(buyables, travelActionRefactorio);
+        refactorioOne.at(15, 5).setGround(terminalRefactorio);
 
         // Add player with balance
-        Player player = new Player("Intern", '@', 42
-        );
+        Player player = new Player("Intern", '@', 4);
         player.addBalance(1000);
-        player.addItemToInventory(new PotOfGold());
-        player.addItemToInventory(new ToiletPaperRoll());
-        world.addPlayer(player, gameMap.at(6, 9));
+        world.addPlayer(player, polymorphiaOne.at(15, 6));
 
         // Add large bolt and metal sheet
         Item largeBolt = new LargeBolt();
-        gameMap.at(8, 7).addItem(largeBolt);
+        polymorphiaOne.at(8, 7).addItem(largeBolt);
 
         Item metalSheet = new MetalSheet();
-        gameMap.at(9, 7).addItem(metalSheet);
+        polymorphiaOne.at(9, 7).addItem(metalSheet);
 
         // Add metal pipe
         Item metalPipe = new MetalPipe();
-        gameMap.at(10, 7).addItem(metalPipe);
+        polymorphiaOne.at(10, 7).addItem(metalPipe);
 
         // Add jar of pickles
         Item jarOfPicklesTwo = new JarOfPickles();
-        gameMap.at(15, 10).addItem(jarOfPicklesTwo);
+        polymorphiaOne.at(15, 10).addItem(jarOfPicklesTwo);
 
         Item jarOfPicklesThree = new JarOfPickles();
-        gameMap.at(14, 10).addItem(jarOfPicklesThree);
+        polymorphiaOne.at(14, 10).addItem(jarOfPicklesThree);
 
         Item jarOfPicklesFour = new JarOfPickles();
-        gameMap.at(13, 10).addItem(jarOfPicklesFour);
+        polymorphiaOne.at(13, 10).addItem(jarOfPicklesFour);
 
         // Create craters spawning HuntsmanSpiders
         Crater craterOne = new Crater(new HuntsmanSpiderSpawner());
-        gameMap.at(26, 4).setGround(craterOne);
+        polymorphiaOne.at(26, 4).setGround(craterOne);
 
         // Create craters spawning AlienBugs
         Crater craterTwo = new Crater(new AlienBugSpawner());
-        gameMap.at(6, 5).setGround(craterTwo);
+        polymorphiaOne.at(6, 5).setGround(craterTwo);
 
         Crater craterThree = new Crater(new AlienBugSpawner());
-        gameMap.at(26, 12).setGround(craterThree);
+        polymorphiaOne.at(26, 12).setGround(craterThree);
 
         // Create craters spawning SuspiciousAstronauts
         Crater craterFour = new Crater(new SuspiciousAstronautSpawner());
-        gameMap.at(26, 13).setGround(craterFour);
-
-        Humanoid humanoid = new Humanoid();
-        gameMap.at(5,9).addActor(humanoid);
+        polymorphiaOne.at(26, 13).setGround(craterFour);
 
         world.run();
     }
